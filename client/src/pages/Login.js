c587d82cdcfdc415d5d7ffc7d10282de37e35573
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-11-20 14:13:12 -0500

    add thoughts and reacitons

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN_USER);
+
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -17,20 +19,25 @@ const Login = (props) => {
   };
 
   // submit form
-// submit form
-const handleFormSubmit = async event => {
-  event.preventDefault();
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-  try {
-    const { data } = await login({
-      variables: { ...formState }
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
     });
-  
-    Auth.login(data.login.token);
-  } catch (e) {
-    console.error(e);
-  }
-};
+  };
 
   return (
     <main className="flex-row justify-center mb-4">
@@ -60,8 +67,9 @@ const handleFormSubmit = async event => {
               <button className="btn d-block w-100" type="submit">
                 Submit
               </button>
-              {error && <div>Login failed</div>}
             </form>
+
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
