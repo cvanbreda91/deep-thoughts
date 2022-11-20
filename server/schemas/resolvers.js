1885ecfab96d1e3672b0ee75c4ab5e6d8b5a12db
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-11-19 23:05:33 -0500

    set up client side and install concurrently to run both the server and client modules at same time

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -39,27 +39,27 @@ const resolvers = {
 
   Mutation: {
     addUser: async (parent, args) => {
-        const user = await User.create(args);
-        const token = signToken(user);
-      
-        return { token, user };
-      },
-      login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-      
-        if (!user) {
-          throw new AuthenticationError('Incorrect credentials');
-        }
-      
-        const correctPw = await user.isCorrectPassword(password);
-      
-        if (!correctPw) {
-          throw new AuthenticationError('Incorrect credentials');
-        }
-      
-        const token = signToken(user);
-        return { token, user };
-      },
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
     addThought: async (parent, args, context) => {
       if (context.user) {
         const thought = await Thought.create({ ...args, username: context.user.username });
